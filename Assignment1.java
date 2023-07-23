599746132ba8ef1cd30f2f19644def68abf8a02b
shreyasawant575 <139642112+shreyasawant575@users.noreply.github.com>
2023-07-23 17:39:08 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,40 @@
+// print hello world
+ class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}   
+// Pimitive Datatypes
+
+class Helloworld {
+    public static void main(String[] args) {
+        int  ab = 20 + 60;
+        System.out.println(20 + 60);
+        char a= 30 + 60;
+        System.out.println(a);
+        boolean a= true;
+        System.out.println(a);float d= 65.676767f;
+        System.out.println(d); 
+        long c=56;
+        System.out.println(c);
+        short a=56;
+        System.out.println(a);
+        double d= 65.676767676767676f;
+        System.out.println(d);
+        // Operators
+        class Helloworld {
+    public static void main(String[] args) {
+       int  a= 50;
+       int b=3;
+    System.out.println(a + b);
+    int c=50;
+    int d=3;
+    System.out.println(c - d);
+    int e=50;
+    int f=3;
+    System.out.println(e * f);
+    int g=50;
+    int h=3;
+    System.out.println(g / h);
+     }
+}
